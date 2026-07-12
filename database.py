5f4e790c268d60999c63400ac4e498c2af09ecc4
Luis Hernandez <wychox3@gmail.com>
2026-07-12 15:21:08 -0600

    feat: creo conexion y esquema de base de datos SQLite

---
+++ database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+NOMBRE_D8 = "Estudiantes.db"
+
+def conectar():
+    """Crea y duevuelve una conexión a la base de datos."""
+    return sqlite3.connect(NOMBRE_D8)
+
+def crear_tabla():
+    """Crea la tabla de estudiantes si no existe."""
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS estudiantes (
+            id INTERGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            edad INTERGER NOT NULL,
+            carrera TEXT NOT NULL,
+            fecha_inscripcion TEXT NOT NULL
+        )
+""")
+
+    conexion.commit()
+    conexion.close()
